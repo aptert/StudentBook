afbeff67380d5c8b349fc2c4ab0b7346c817382f
tomapter <tom@eccmperformance.com>
2017-10-24 15:16:29 +0200

    dbmanager

--- src/Model/DBManager.java
+++ src/Model/DBManager.java
@@ -1,5 +1,17 @@
 package Model;
 
-public class DBManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBManager {
+	public static Connection Connector(){
+		try{
+			Connection connection = DriverManager.getConnection("jdbc:sqlite:Users\\Nada-\\Des ktop\\TutorielFXDB.sqlite");
+			return connection;
+			}
+		catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
